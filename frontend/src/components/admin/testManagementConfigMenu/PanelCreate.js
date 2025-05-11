a2bcf423371ee477ff99bbb0f119d374231487a2
Aditya @ArchLinux <132184385+adityadeshlahre@users.noreply.github.com>
2025-05-11 23:42:07 +0530

    feat(PMCN): create new panel page init

--- frontend/src/components/admin/testManagementConfigMenu/PanelCreate.js
+++ frontend/src/components/admin/testManagementConfigMenu/PanelCreate.js
@@ -24,6 +24,7 @@ import {
   Stack,
   UnorderedList,
   ListItem,
+  TextInput,
 } from "@carbon/react";
 import {
   getFromOpenElisServer,
@@ -64,35 +65,170 @@ function PanelCreate() {
     useContext(NotificationContext);
 
   const intl = useIntl();
+  const [isLoading, setIsLoading] = useState(true);
+  const [bothFilled, setBothFilled] = useState(false);
+  const [englishLangPost, setEnglishLangPost] = useState("");
+  const [frenchLangPost, setFrenchLangPost] = useState("");
+  const [inputError, setInputError] = useState(false);
+  const [panelCreateList, setPanelCreateList] = useState({});
 
   const componentMounted = useRef(false);
+
+  const handlePanelCreateList = (response) => {
+    if (!response) {
+      setIsLoading(true);
+    } else {
+      setPanelCreateList(response);
+    }
+  };
+
+  const handlePanelCreateListCall = () => {};
+
+  useEffect(() => {
+    componentMounted.current = true;
+    setIsLoading(true);
+    getFromOpenElisServer(`/rest/PanelCreate`, handlePanelCreateList);
+    return () => {
+      componentMounted.current = false;
+      setIsLoading(false);
+    };
+  }, []);
+
+  if (!isLoading) {
+    return (
+      <>
+        <Loading />
+      </>
+    );
+  }
 
   return (
     <>
       {notificationVisible === true ? <AlertDialog /> : ""}
       <div className="adminPageContent">
         <PageBreadCrumb breadcrumbs={breadcrumbs} />
-        <Grid fullWidth={true}>
-          <Column lg={16} md={8} sm={4}>
-            <Section>
-              <Heading>
-                <FormattedMessage id="banner.menu.patientEdit" />
-              </Heading>
-            </Section>
-          </Column>
-        </Grid>
-        <br />
-        <hr />
-        <br />
-        <Grid fullWidth={true}>
-          <Column lg={16} md={8} sm={4}>
-            <Section>
-              <Heading>
-                <FormattedMessage id="configuration.panel.create" />
-              </Heading>
-            </Section>
-          </Column>
-        </Grid>
+        <div className="orderLegendBody">
+          <Grid fullWidth={true}>
+            <Column lg={16} md={8} sm={4}>
+              <Section>
+                <Heading>
+                  <FormattedMessage id="banner.menu.patientEdit" />
+                </Heading>
+              </Section>
+            </Column>
+          </Grid>
+          <br />
+          <hr />
+          <br />
+          <Grid fullWidth={true}>
+            <Column lg={16} md={8} sm={4}>
+              <Section>
+                <Heading>
+                  <FormattedMessage id="configuration.panel.create" />
+                </Heading>
+              </Section>
+            </Column>
+          </Grid>
+          <br />
+          <hr />
+          <br />
+          <Grid fullWidth={true}>
+            <Column lg={16} md={8} sm={4}>
+              <Section>
+                <Section>
+                  <Section>
+                    <Heading>
+                      <FormattedMessage id="panel.new" />
+                    </Heading>
+                  </Section>
+                </Section>
+              </Section>
+            </Column>
+          </Grid>
+          <br />
+          <hr />
+          <br />
+          <Grid fullWidth={true}>
+            <Column lg={8} md={4} sm={4}>
+              <>
+                <FormattedMessage id="english.label" />
+                <span className="requiredlabel">*</span> :
+              </>
+            </Column>
+            <Column lg={8} md={4} sm={4}>
+              <TextInput
+                id={`eng`}
+                labelText=""
+                hideLabel
+                disabled={bothFilled}
+                value={`${englishLangPost}` || ""}
+                onChange={(e) => {
+                  setEnglishLangPost(e.target.value);
+                  setInputError(false);
+                }}
+                required
+                invalid={inputError}
+                invalidText={<FormattedMessage id="required.invalidtext" />}
+              />
+            </Column>
+            <Column lg={8} md={4} sm={4}>
+              <>
+                <FormattedMessage id="french.label" />
+                <span className="requiredlabel">*</span> :
+              </>
+            </Column>
+            <Column lg={8} md={4} sm={4}>
+              <TextInput
+                id={`fr`}
+                labelText=""
+                hideLabel
+                disabled={bothFilled}
+                value={`${frenchLangPost}` || ""}
+                onChange={(e) => {
+                  setFrenchLangPost(e.target.value);
+                  setInputError(false);
+                }}
+                required
+                invalid={inputError}
+                invalidText={<FormattedMessage id="required.invalidtext" />}
+              />
+            </Column>
+          </Grid>
+          <br />
+          <Grid fullWidth={true}>
+            <Column lg={8} md={8} sm={4}>
+              <Button
+                onClick={() => {
+                  handlePanelCreateListCall();
+                  setBothFilled(true);
+                  setInputError(false);
+                }}
+                type="button"
+                kind="primary"
+              >
+                <FormattedMessage id="next.action.button" />
+              </Button>{" "}
+              <Button
+                type="button"
+                kind="tertiary"
+                onClick={() => {
+                  window.location.reload();
+                }}
+              >
+                <FormattedMessage id="label.button.previous" />
+              </Button>
+            </Column>
+          </Grid>
+          <br />
+          <hr />
+        </div>
+        <button
+          onClick={() => {
+            console.log(panelCreateList);
+          }}
+        >
+          panelCreateList
+        </button>
       </div>
     </>
   );
