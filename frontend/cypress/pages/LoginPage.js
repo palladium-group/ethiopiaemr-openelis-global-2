c8c53f1e2860c7564fc076dee63a5556e3ec5e5b
Bahati308 <bahatibrianp@gmail.com>
2025-05-14 23:37:43 +0300

    Login fix

--- frontend/cypress/pages/LoginPage.js
+++ frontend/cypress/pages/LoginPage.js
@@ -27,6 +27,7 @@ class LoginPage {
 
   enterUsername(value) {
     this.getUsernameElement().should("be.visible");
+    cy.wait(200);
     this.getUsernameElement().type(value);
     this.getUsernameElement().should("have.value", value);
   }
