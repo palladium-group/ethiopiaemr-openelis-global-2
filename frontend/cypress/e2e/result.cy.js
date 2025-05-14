1dbeb67f44dad54b33ee689f68f7cb3d1604ad26
Bahati308 <bahatibrianp@gmail.com>
2025-05-14 12:33:14 +0300

    Added wait

--- frontend/cypress/e2e/result.cy.js
+++ frontend/cypress/e2e/result.cy.js
@@ -348,6 +348,8 @@ describe("Result By Test And Status", function () {
       result.selectTestName(order.testName);
       result.searchResults();
       result.expandSampleDetails();
+    });
+    cy.fixture("result").then((res) => {
       result.selectTestMethod(res.pcrTestMethod);
     });
     result.submitResults();
