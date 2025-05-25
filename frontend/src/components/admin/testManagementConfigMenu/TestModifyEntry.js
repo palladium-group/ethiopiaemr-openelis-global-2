56930ea7423ce9bedb0e93d79f9a39d2be3c83ce
Aditya @ArchLinux <132184385+adityadeshlahre@users.noreply.github.com>
2025-05-25 11:58:15 +0530

    feat(TME): custom tooggle component

--- frontend/src/components/admin/testManagementConfigMenu/TestModifyEntry.js
+++ frontend/src/components/admin/testManagementConfigMenu/TestModifyEntry.js
@@ -23,6 +23,7 @@ import {
   SelectItem,
   Stack,
   ClickableTile,
+  Toggle,
 } from "@carbon/react";
 import {
   getFromOpenElisServer,
@@ -40,6 +41,7 @@ import { FormattedMessage, injectIntl, useIntl } from "react-intl";
 import PageBreadCrumb from "../../common/PageBreadCrumb.js";
 import CustomCheckBox from "../../common/CustomCheckBox.js";
 import ActionPaginationButtonType from "../../common/ActionPaginationButtonType.js";
+import { CustomShowGuide } from "./customComponents/CustomShowGuide.js";
 
 let breadcrumbs = [
   { label: "home.label", link: "/" },
@@ -61,8 +63,13 @@ function TestModifyEntry() {
   const intl = useIntl();
   const [isLoading, setIsLoading] = useState(false);
   const [testMonifyList, setTestModifyList] = useState({});
+  const [showGuide, setShowGuide] = useState(false);
 
   const componentMounted = useRef(false);
+
+  const handleToggleShowGuide = () => {
+    setShowGuide(!showGuide);
+  };
 
   const handleTestModifyEntryList = (res) => {
     if (!res) {
@@ -89,6 +96,93 @@ function TestModifyEntry() {
       </>
     );
   }
+
+  const rows = [
+    {
+      id: "name",
+      field: intl.formatMessage({ id: "field.name" }),
+      description: <FormattedMessage id="test.description.name" />,
+    },
+    {
+      id: "reportName",
+      field: intl.formatMessage({ id: "field.reportName" }),
+      description: <FormattedMessage id="test.description.reportName" />,
+    },
+    {
+      id: "testSection",
+      field: intl.formatMessage({ id: "field.testSection" }),
+      description: <FormattedMessage id="test.description.testSection" />,
+    },
+    {
+      id: "panel",
+      field: intl.formatMessage({ id: "field.panel" }),
+      description: <FormattedMessage id="test.description.panel" />,
+    },
+    {
+      id: "uom",
+      field: intl.formatMessage({ id: "field.uom" }),
+      description: <FormattedMessage id="test.description.uom" />,
+    },
+    {
+      id: "resultType",
+      field: intl.formatMessage({ id: "field.resultType" }),
+      description: (
+        <>
+          <p>
+            <FormattedMessage id="description.resultType.kind" />
+          </p>
+          <ul>
+            <li>
+              <strong>
+                <FormattedMessage id="description.resultType.numeric" />
+              </strong>
+              <FormattedMessage id="description.resultType.numericDesc" />
+            </li>
+            <li>
+              <strong>
+                <FormattedMessage id="description.resultType.alphanumeric" />
+              </strong>
+              <FormattedMessage id="description.resultType.alphanumericDesc" />
+            </li>
+            <li>
+              <strong>
+                <FormattedMessage id="description.resultType.textArea" />
+              </strong>
+              <FormattedMessage id="description.resultType.textAreaDesc" />
+            </li>
+            <li>
+              <strong>
+                <FormattedMessage id="description.resultType.selectList" />
+              </strong>
+              <FormattedMessage id="description.resultType.selectListDesc" />
+            </li>
+            <li>
+              <strong>
+                <FormattedMessage id="description.resultType.multiSelectList" />
+              </strong>
+              <FormattedMessage id="description.resultType.multiSelectListDesc" />
+            </li>
+            <li>
+              <strong>
+                <FormattedMessage id="description.resultType.cascadingMultiSelectList" />
+              </strong>
+              <FormattedMessage id="description.resultType.cascadingMultiSelectListDesc" />
+            </li>
+          </ul>
+        </>
+      ),
+    },
+    {
+      id: "active",
+      field: intl.formatMessage({ id: "test.field.active" }),
+      description: <FormattedMessage id="test.description.active" />,
+    },
+    {
+      id: "orderable",
+      field: intl.formatMessage({ id: "test.field.orderable" }),
+      description: <FormattedMessage id="test.description.orderable" />,
+    },
+  ];
 
   return (
     <>
@@ -132,6 +226,28 @@ function TestModifyEntry() {
               <></>
             )}
           </Grid>
+          <br />
+          <Grid fullWidth={true}>
+            <Column lg={16} md={8} sm={4}>
+              <Section>
+                <Section>
+                  <Section>
+                    <Heading>
+                      <FormattedMessage id="label.viewtestCatalog" />
+                    </Heading>
+                  </Section>
+                </Section>
+              </Section>
+            </Column>
+            <Column lg={16} md={8} sm={4}>
+              <Toggle
+                id="toggle"
+                labelText="Show Guide"
+                onClick={handleToggleShowGuide}
+              />
+            </Column>
+          </Grid>
+          {showGuide && <CustomShowGuide rows={rows} />}
         </div>
         <button
           onClick={() => {
