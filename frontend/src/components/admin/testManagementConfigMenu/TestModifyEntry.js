89652930cf0cde8d4326383623dc4c22150ab479
Aditya @ArchLinux <132184385+adityadeshlahre@users.noreply.github.com>
2025-05-25 10:52:56 +0530

    feat(TME): initial tests display UI init + page enabled

--- frontend/src/components/admin/testManagementConfigMenu/TestModifyEntry.js
+++ frontend/src/components/admin/testManagementConfigMenu/TestModifyEntry.js
@@ -22,6 +22,7 @@ import {
   Select,
   SelectItem,
   Stack,
+  ClickableTile,
 } from "@carbon/react";
 import {
   getFromOpenElisServer,
@@ -58,23 +59,87 @@ function TestModifyEntry() {
     useContext(NotificationContext);
 
   const intl = useIntl();
+  const [isLoading, setIsLoading] = useState(false);
+  const [testMonifyList, setTestModifyList] = useState({});
 
   const componentMounted = useRef(false);
+
+  const handleTestModifyEntryList = (res) => {
+    if (!res) {
+      setIsLoading(true);
+    } else {
+      setTestModifyList(res);
+    }
+  };
+
+  useEffect(() => {
+    componentMounted.current = true;
+    setIsLoading(true);
+    getFromOpenElisServer(`/rest/TestModifyEntry`, handleTestModifyEntryList);
+    return () => {
+      componentMounted.current = false;
+      setIsLoading(false);
+    };
+  }, []);
+
+  if (!isLoading) {
+    return (
+      <>
+        <Loading />
+      </>
+    );
+  }
 
   return (
     <>
       {notificationVisible === true ? <AlertDialog /> : ""}
       <div className="adminPageContent">
         <PageBreadCrumb breadcrumbs={breadcrumbs} />
-        <Grid fullWidth={true}>
-          <Column lg={16} md={8} sm={4}>
-            <Section>
-              <Heading>
-                <FormattedMessage id="configuration.test.modify" />
-              </Heading>
-            </Section>
-          </Column>
-        </Grid>
+        <div className="orderLegendBody">
+          <Grid fullWidth={true}>
+            <Column lg={16} md={8} sm={4}>
+              <Section>
+                <Heading>
+                  <FormattedMessage id="configuration.test.modify" />
+                </Heading>
+              </Section>
+            </Column>
+          </Grid>
+          <br />
+          <Grid fullWidth={true}>
+            {testMonifyList && testMonifyList?.testList?.length > 0 ? (
+              <>
+                {testMonifyList?.testList?.map((test) => (
+                  <Column
+                    style={{ margin: "2px" }}
+                    lg={4}
+                    md={4}
+                    sm={2}
+                    key={test.id}
+                  >
+                    <ClickableTile
+                      id={test.id}
+                      onClick={() => {
+                        console.log(test.id);
+                      }}
+                    >
+                      {test.value}
+                    </ClickableTile>
+                  </Column>
+                ))}
+              </>
+            ) : (
+              <></>
+            )}
+          </Grid>
+        </div>
+        <button
+          onClick={() => {
+            console.log(testMonifyList);
+          }}
+        >
+          testMonifyList
+        </button>
       </div>
     </>
   );
