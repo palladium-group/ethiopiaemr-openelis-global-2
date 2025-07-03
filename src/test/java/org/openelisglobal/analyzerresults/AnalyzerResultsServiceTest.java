d4ac5587f93df9020eac26122857296fc7c3e48a
Atwijukire Ariho Seth <120330466+Ariho-Seth@users.noreply.github.com>
2025-07-03 04:15:29 +0100

    Adding tests for the AnalyzerResults service Class
    Signed-off-by: Atwijukire Ariho Seth <120330466+Ariho-Seth@users.noreply.github.com>

--- src/test/java/org/openelisglobal/analyzerresults/AnalyzerResultsServiceTest.java
+++ src/test/java/org/openelisglobal/analyzerresults/AnalyzerResultsServiceTest.java
@@ -1,6 +1,9 @@
 package org.openelisglobal.analyzerresults;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertFalse;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
