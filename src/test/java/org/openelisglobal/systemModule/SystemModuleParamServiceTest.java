0d045b84966ce9845eb7731166574dc6e7a10d18
Atwijukire Ariho Seth <120330466+Ariho-Seth@users.noreply.github.com>
2025-07-12 23:14:55 +0100

    Added tests for the SystemModuleParam service class
    Signed-off-by: Atwijukire Ariho Seth <120330466+Ariho-Seth@users.noreply.github.com>

--- src/test/java/org/openelisglobal/systemModule/SystemModuleParamServiceTest.java
+++ src/test/java/org/openelisglobal/systemModule/SystemModuleParamServiceTest.java
@@ -1,7 +1,7 @@
 package org.openelisglobal.systemModule;
 
-import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
