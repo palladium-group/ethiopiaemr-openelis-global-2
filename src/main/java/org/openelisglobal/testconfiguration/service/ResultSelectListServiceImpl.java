983329e02c7f5a0f05af85879de71d5095477b7b
Atwijukire Ariho Seth <120330466+Ariho-Seth@users.noreply.github.com>
2025-07-21 21:09:50 +0100

    Adding integration tests fot the testconfiguration services
    Signed-off-by: Atwijukire Ariho Seth <120330466+Ariho-Seth@users.noreply.github.com>

--- src/main/java/org/openelisglobal/testconfiguration/service/ResultSelectListServiceImpl.java
+++ src/main/java/org/openelisglobal/testconfiguration/service/ResultSelectListServiceImpl.java
@@ -123,8 +123,8 @@ public class ResultSelectListServiceImpl implements ResultSelectListService {
                 tests = (JSONArray) parsed;
             } else if (parsed instanceof JSONObject) {
                 JSONObject obj = (JSONObject) parsed;
-                String testsStr = (String) obj.get("tests");
-                tests = (JSONArray) parser.parse(testsStr);
+                tests = (JSONArray) obj.get("tests");
+
             } else {
                 throw new IllegalArgumentException("Invalid testSelectListJson format");
             }
--- src/test/java/org/openelisglobal/AppTestConfig.java
+++ src/test/java/org/openelisglobal/AppTestConfig.java
@@ -80,8 +80,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
         "org.openelisglobal.action", "org.openelisglobal.analysisqaevent", "org.openelisglobal.analysisqaeventaction",
         "org.openelisglobal.dataexchange", "org.openelisglobal.samplepdf", "org.openelisglobal.samplenewborn",
         "org.openelisglobal.sampleqaeventaction", "org.openelisglobal.analyzerresults", "org.openelisglobal.testreflex",
-        "org.openelisglobal.county", "org.openelisglobal.sampletracking",
-        "org.openelisglobal.projectorganization" }, excludeFilters = {
+        "org.openelisglobal.county", "org.openelisglobal.sampletracking", "org.openelisglobal.projectorganization",
+        "org.openelisglobal.testconfiguration" }, excludeFilters = {
                 @ComponentScan.Filter(type = FilterType.REGEX, pattern = "org.openelisglobal.patient.controller.*"),
                 @ComponentScan.Filter(type = FilterType.REGEX, pattern = "org.openelisglobal.organization.controller.*"),
                 @ComponentScan.Filter(type = FilterType.REGEX, pattern = "org.openelisglobal.sample.controller.*"),
