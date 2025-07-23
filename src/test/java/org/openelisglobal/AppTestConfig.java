ce739dbec9924aac24fc47395b2bef8f497b1cae
Atwijukire Ariho Seth <120330466+Ariho-Seth@users.noreply.github.com>
2025-07-23 10:44:01 +0100

    code refactoring
    Signed-off-by: Atwijukire Ariho Seth <120330466+Ariho-Seth@users.noreply.github.com>

--- src/test/java/org/openelisglobal/AppTestConfig.java
+++ src/test/java/org/openelisglobal/AppTestConfig.java
@@ -81,8 +81,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
         "org.openelisglobal.dataexchange", "org.openelisglobal.samplepdf", "org.openelisglobal.samplenewborn",
         "org.openelisglobal.sampleqaeventaction", "org.openelisglobal.analyzerresults", "org.openelisglobal.testreflex",
         "org.openelisglobal.county", "org.openelisglobal.sampletracking", "org.openelisglobal.testresultsview",
-        "org.openelisglobal.projectorganization", "org.openelisglobal.sourceofsample", 
-                               "org.openelisglobal.testconfiguration" }, excludeFilters = {
+        "org.openelisglobal.projectorganization", "org.openelisglobal.sourceofsample",
+        "org.openelisglobal.testconfiguration" }, excludeFilters = {
 
                 @ComponentScan.Filter(type = FilterType.REGEX, pattern = "org.openelisglobal.patient.controller.*"),
                 @ComponentScan.Filter(type = FilterType.REGEX, pattern = "org.openelisglobal.organization.controller.*"),
