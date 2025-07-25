a52eef9cb338124c060150384188ca31e70e0ac3
Bahati308 <bahatibrianp@gmail.com>
2025-07-25 12:53:50 +0300

    BatchTestReassignment

---
+++ frontend/cypress/e2e/AdminE2E/batchTestReassignmentandCancelation.cy.js
@@ -0,0 +1,31 @@
+import LoginPage from "../../pages/LoginPage";
+
+describe("Batch Test Reassignment and Canelation", function () {
+  let homePage, loginPage, adminPage, batchTestPage;
+
+  before(() => {
+    loginPage = new LoginPage();
+    loginPage.visit();
+    homePage = loginPage.goToHomePage();
+    adminPage = homePage.goToAdminPage();
+  });
+
+  it("Navigate to Batch Test Reassignment and Canelation Page", () => {
+    batchTestPage = adminPage.goToBatchTestReassignmentandCanelationPage();
+    batchTestPage.validatePageTitle();
+  });
+
+  describe("Enter Data and Save", () => {
+    it("Select Sample and Tests", () => {
+      batchTestPage.selectSampleType();
+      batchTestPage.chechBoxes("currentTest");
+      batchTestPage.chechBoxes("replaceWith");
+      batchTestPage.selectTest("1");
+      batchTestPage.selectTest("0");
+    });
+
+    it("Save Changes", () => {
+      batchTestPage.clickOk();
+    });
+  });
+});
