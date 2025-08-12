04449218617520796e2478de0745d4aadf3d60a2
Mutesasira Moses <mozzymutesa@gmail.com>
2025-08-12 18:04:20 +0300

    Revert "Revert "(test) TestReflex service integration test""

--- src/test/java/org/openelisglobal/AppTestConfig.java
+++ src/test/java/org/openelisglobal/AppTestConfig.java
@@ -82,7 +82,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
         "org.openelisglobal.sampleqaeventaction", "org.openelisglobal.analyzerresults", "org.openelisglobal.testreflex",
         "org.openelisglobal.county", "org.openelisglobal.sampletracking", "org.openelisglobal.testresultsview",
         "org.openelisglobal.projectorganization", "org.openelisglobal.sourceofsample",
-        "org.openelisglobal.testconfiguration", "org.openelisglobal.usertestsection" }, excludeFilters = {
+        "org.openelisglobal.testconfiguration", "org.openelisglobal.usertestsection",
+        "org.openelisglobal.testcalculated" }, excludeFilters = {
 
                 @ComponentScan.Filter(type = FilterType.REGEX, pattern = "org.openelisglobal.patient.controller.*"),
                 @ComponentScan.Filter(type = FilterType.REGEX, pattern = "org.openelisglobal.organization.controller.*"),
