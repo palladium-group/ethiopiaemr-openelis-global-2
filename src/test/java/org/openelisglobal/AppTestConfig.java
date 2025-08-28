vickabire/OpenELIS-Global-2

suppress odoo configs in test environment

src/test/java/org/openelisglobal/AppTestConfig.java
@@ -27,6 +27,8 @@ import org.openelisglobal.externalconnections.service.ExternalConnectionService;
 import org.openelisglobal.internationalization.MessageUtil;
 import org.openelisglobal.notification.service.AnalysisNotificationConfigService;
 import org.openelisglobal.notification.service.TestNotificationConfigService;
+import org.openelisglobal.odoo.client.OdooClient;
+import org.openelisglobal.odoo.config.TestProductMapping;
 import org.openelisglobal.organization.service.OrganizationTypeService;
 import org.openelisglobal.referral.fhir.service.FhirReferralService;
 import org.openelisglobal.reports.service.WHONetReportServiceImpl;
@@ -82,8 +84,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
         "org.openelisglobal.sampleqaeventaction", "org.openelisglobal.analyzerresults", "org.openelisglobal.testreflex",
         "org.openelisglobal.county", "org.openelisglobal.sampletracking", "org.openelisglobal.testresultsview",
         "org.openelisglobal.projectorganization", "org.openelisglobal.sourceofsample",
-        "org.openelisglobal.testconfiguration", "org.openelisglobal.usertestsection" }, excludeFilters = {
-
+        "org.openelisglobal.testconfiguration", "org.openelisglobal.usertestsection",
+        "org.openelisglobal.county", "org.openelisglobal.odoo" }, excludeFilters = {
                 @ComponentScan.Filter(type = FilterType.REGEX, pattern = "org.openelisglobal.patient.controller.*"),
                 @ComponentScan.Filter(type = FilterType.REGEX, pattern = "org.openelisglobal.organization.controller.*"),
                 @ComponentScan.Filter(type = FilterType.REGEX, pattern = "org.openelisglobal.sample.controller.*"),
@@ -258,6 +260,18 @@ public class AppTestConfig implements WebMvcConfigurer {
     @Profile("test")
     public IStatusService iStatusService() {
         return mock(IStatusService.class);
+    }
+
+    @Bean
+    @Profile("test")
+    public OdooClient odooClient() {
+        return mock(OdooClient.class);
+    }
+
+    @Bean
+    @Profile("test")
+    public TestProductMapping testProductMapping() {
+        return mock(TestProductMapping.class);
     }
 
     @Bean()

palladium-group/ethiopiaemr-openelis-global-2

src/test/java/org/openelisglobal/AppTestConfig.java
@@ -27,6 +27,8 @@ import org.openelisglobal.externalconnections.service.ExternalConnectionService;
 import org.openelisglobal.internationalization.MessageUtil;
 import org.openelisglobal.notification.service.AnalysisNotificationConfigService;
 import org.openelisglobal.notification.service.TestNotificationConfigService;
+import org.openelisglobal.odoo.client.OdooClient;
+import org.openelisglobal.odoo.config.TestProductMapping;
 import org.openelisglobal.organization.service.OrganizationTypeService;
 import org.openelisglobal.referral.fhir.service.FhirReferralService;
 import org.openelisglobal.reports.service.WHONetReportServiceImpl;
@@ -83,7 +85,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
         "org.openelisglobal.county", "org.openelisglobal.sampletracking", "org.openelisglobal.testresultsview",
         "org.openelisglobal.projectorganization", "org.openelisglobal.sourceofsample",
         "org.openelisglobal.testconfiguration", "org.openelisglobal.usertestsection",
-        "org.openelisglobal.testcalculated" }, excludeFilters = {
+        "org.openelisglobal.testcalculated", "org.openelisglobal.odoo" }, excludeFilters = {
 
                 @ComponentScan.Filter(type = FilterType.REGEX, pattern = "org.openelisglobal.patient.controller.*"),
                 @ComponentScan.Filter(type = FilterType.REGEX, pattern = "org.openelisglobal.organization.controller.*"),
@@ -259,6 +261,18 @@ public class AppTestConfig implements WebMvcConfigurer {
     @Profile("test")
     public IStatusService iStatusService() {
         return mock(IStatusService.class);
+    }
+
+    @Bean
+    @Profile("test")
+    public OdooClient odooClient() {
+        return mock(OdooClient.class);
+    }
+
+    @Bean
+    @Profile("test")
+    public TestProductMapping testProductMapping() {
+        return mock(TestProductMapping.class);
     }
 
     @Bean()