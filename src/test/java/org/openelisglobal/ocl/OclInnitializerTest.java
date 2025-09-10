0f06045d689abad04f99b36dbefb7949d1483a42
mozzy11 <mozzymutesa@gmail.com>
2025-09-10 19:47:45 +0300

    map sample type by localized name

--- src/test/java/org/openelisglobal/ocl/OclInnitializerTest.java
+++ src/test/java/org/openelisglobal/ocl/OclInnitializerTest.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import org.junit.Before;
 import org.junit.Test;
 import org.openelisglobal.BaseWebContextSensitiveTest;
+import org.openelisglobal.localization.service.LocalizationService;
+import org.openelisglobal.typeofsample.service.TypeOfSampleService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,10 +21,18 @@ public class OclInnitializerTest extends BaseWebContextSensitiveTest {
     @Autowired
     OclImportInitializer oclImportInitializer;
 
+    @Autowired
+    TypeOfSampleService typeOfSampleService;
+
+    @Autowired
+    LocalizationService localizationService;
+
     private static String oclDirPath;
+    private static String sampleType = "Whole Blood";
 
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
+        executeDataSetWithStateManagement("testdata/ocl-import.xml");
         if (oclZipImporter == null) {
             fail("OclZipImporter bean not autowired. Check Spring configuration.");
         }
@@ -34,5 +44,4 @@ public class OclInnitializerTest extends BaseWebContextSensitiveTest {
         java.io.File tempFile = java.io.File.createTempFile("ocl_imported", ".flag");
         oclImportInitializer.performOclImport(oclDirPath, tempFile.getAbsolutePath());
     }
-
 }
