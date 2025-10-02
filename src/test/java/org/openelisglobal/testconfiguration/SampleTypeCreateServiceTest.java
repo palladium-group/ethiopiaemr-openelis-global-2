356c900242782282a1d548a92a4b1405e9d0e75e
Agaba-derrick <agabaderrick18@gmail.com>
2025-10-02 17:36:33 +0300

    fix the datasets
    Signed-off-by: Agaba-derrick <agabaderrick18@gmail.com>

--- src/test/java/org/openelisglobal/testconfiguration/SampleTypeCreateServiceTest.java
+++ src/test/java/org/openelisglobal/testconfiguration/SampleTypeCreateServiceTest.java
@@ -104,7 +104,7 @@ public class SampleTypeCreateServiceTest extends BaseWebContextSensitiveTest {
         List<TypeOfSample> newTypeOfSamples = typeOfSampleService.getAll();
 
         assertEquals((initialTypeOfSampleCount + 1), newTypeOfSamples.size());
-        assertEquals("blood tests", newTypeOfSamples.get(initialTypeOfSampleCount).getDescription());
+        assertEquals("Fluidal acids", newTypeOfSamples.get(initialTypeOfSampleCount).getDescription());
 
         List<SystemModule> newSystemModules = systemModuleService.getAll();
         assertEquals(initialSystemModuleCount + 3, newSystemModules.size());
