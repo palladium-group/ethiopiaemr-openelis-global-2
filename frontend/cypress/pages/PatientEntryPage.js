98e01a9cb274ed65c6cd3266349f810bbb8ab086
mozzy11 <mozzymutesa@gmail.com>
2025-10-29 14:40:46 +0300

    fix patient search tests

--- frontend/cypress/pages/PatientEntryPage.js
+++ frontend/cypress/pages/PatientEntryPage.js
@@ -83,6 +83,7 @@ class PatientEntryPage {
   }
 
   clickSearchPatientButton() {
+    cy.get(this.dateOfBirth).should("not.exist");
     cy.getElement("#local_search").should("be.visible").click();
   }
 
