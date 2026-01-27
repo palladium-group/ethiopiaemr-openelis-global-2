663f9206bbf6cf883d1cdf16fcf0c1d32b280947
peach280 <vaishnavibhandari.128@gmail.com>
2026-01-27 08:31:01 +0530

    Replaced inline div to use carbon grid (#1923)
    Signed-off-by: Vaishnavi Bhandari <vaishnavibhandari.128@gmail.com>

--- frontend/src/components/immunohistochemistry/ImmunohistochemistryDashboard.js
+++ frontend/src/components/immunohistochemistry/ImmunohistochemistryDashboard.js
@@ -324,44 +324,52 @@ function ImmunohistochemistryDashboard() {
               })}
             />
           </Column>
-          <Column lg={8} md={4} sm={2}>
-            <div className="inlineDivBlock">
-              <div>Filters:</div>
-              <Checkbox
-                labelText={intl.formatMessage({
-                  id: "label.filters.mycases",
-                })}
-                id="filterMyCases"
-                value={filters.myCases}
-                onChange={(e) =>
-                  setFilters({ ...filters, myCases: e.target.checked })
-                }
-              />
-              <Select
-                id="statusFilter"
-                name="statusFilter"
-                labelText={intl.formatMessage({
-                  id: "label.filters.status",
-                })}
-                value={
-                  filters.statuses.length > 1 ? "All" : filters.statuses[0].id
-                }
-                onChange={setStatusFilter}
-                noLabel
-              >
-                <SelectItem disabled value="placeholder" text="Status" />
-                <SelectItem text="All" value="All" />
-                {statuses.map((status, index) => {
-                  return (
-                    <SelectItem
-                      key={index}
-                      text={status.value}
-                      value={status.id}
-                    />
-                  );
-                })}
-              </Select>
-            </div>
+          <Column lg={8} md={4} sm={4}>
+            <Grid fullWidth={true}>
+              <Column lg={1} md={1} sm={1}>
+                <div style={{ marginTop: "9px" }}>
+                  <FormattedMessage id="filters.label" />
+                </div>
+              </Column>
+              <div style={{ marginTop: "4px" }}>
+                <Checkbox
+                  labelText={intl.formatMessage({
+                    id: "label.filters.mycases",
+                  })}
+                  id="filterMyCases"
+                  value={filters.myCases}
+                  onChange={(e) =>
+                    setFilters({ ...filters, myCases: e.target.checked })
+                  }
+                />
+              </div>
+              <Column lg={3} md={2} sm={2}>
+                <Select
+                  id="statusFilter"
+                  name="statusFilter"
+                  labelText={intl.formatMessage({
+                    id: "label.filters.status",
+                  })}
+                  value={
+                    filters.statuses.length > 1 ? "All" : filters.statuses[0].id
+                  }
+                  onChange={setStatusFilter}
+                  noLabel
+                >
+                  <SelectItem disabled value="placeholder" text="Status" />
+                  <SelectItem text="All" value="All" />
+                  {statuses.map((status, index) => {
+                    return (
+                      <SelectItem
+                        key={index}
+                        text={status.value}
+                        value={status.id}
+                      />
+                    );
+                  })}
+                </Select>
+              </Column>
+            </Grid>
           </Column>
 
           <Column lg={16} md={8} sm={4}>
