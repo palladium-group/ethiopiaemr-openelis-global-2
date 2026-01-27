2d3c216871abc02a3708dc1d312e3157135d8ee2
peach280 <vaishnavibhandari.128@gmail.com>
2026-01-27 08:29:48 +0530

    Replaced inline div to use carbon grid (#1922)
    Signed-off-by: Vaishnavi Bhandari <vaishnavibhandari.128@gmail.com>

--- frontend/src/components/admin/reflexTests/ReflexRuleForm.js
+++ frontend/src/components/admin/reflexTests/ReflexRuleForm.js
@@ -465,50 +465,60 @@ function ReflexRule() {
             <Form onSubmit={(e) => handleSubmit(e, index)}>
               <Stack gap={7}>
                 <div className="ruleBody">
-                  <div className="inlineDiv">
-                    <div>
-                      <TextInput
-                        name="ruleName"
-                        className="reflexInputText"
-                        type="text"
-                        id={index + "_rulename"}
-                        labelText={
-                          <FormattedMessage id="rulebuilder.label.ruleName" />
-                        }
-                        value={rule.ruleName}
-                        onChange={(e) => handleRuleFieldChange(e, index)}
-                        required
-                      />
-                    </div>
-                    <div>&nbsp; &nbsp;</div>
-                    <div>
-                      <Toggle
-                        toggled={rule.toggled}
-                        aria-label="toggle button"
-                        id={index + "_toggle"}
-                        labelText={
-                          <FormattedMessage id="rulebuilder.label.toggleRule" />
-                        }
-                        onToggle={(e) => toggleRule(e, index)}
-                        onClick={handleClick}
-                      />
-                    </div>
-                    <div>&nbsp; &nbsp; &nbsp; &nbsp;</div>
-                    <div>
-                      <Checkbox
-                        labelText={"Active: " + rule.active}
-                        name="active"
-                        id={index + "_active"}
-                        checked={rule.active}
-                        disabled={rule.active}
-                        onChange={(e) => {
-                          const list = [...ruleList];
-                          list[index]["active"] = e.target.checked;
-                          setRuleList(list);
-                        }}
-                      />
-                    </div>
-                  </div>
+                  <Grid>
+                    <Column lg={16} md={8} sm={4}>
+                      <Grid>
+                        <Column lg={3} md={2} sm={4}>
+                          <div>
+                            <TextInput
+                              name="ruleName"
+                              className="reflexInputText"
+                              type="text"
+                              id={index + "_rulename"}
+                              labelText={
+                                <FormattedMessage id="rulebuilder.label.ruleName" />
+                              }
+                              value={rule.ruleName}
+                              onChange={(e) => handleRuleFieldChange(e, index)}
+                              required
+                            />
+                          </div>
+                        </Column>
+
+                        <Column lg={1} md={1} sm={4}>
+                          <div>
+                            <Toggle
+                              toggled={rule.toggled}
+                              aria-label="toggle button"
+                              id={index + "_toggle"}
+                              labelText={
+                                <FormattedMessage id="rulebuilder.label.toggleRule" />
+                              }
+                              onToggle={(e) => toggleRule(e, index)}
+                              onClick={handleClick}
+                            />
+                          </div>
+                        </Column>
+                        <div>&nbsp; &nbsp; &nbsp; &nbsp;</div>
+                        <Column lg={5} md={2} sm={4}>
+                          <div>
+                            <Checkbox
+                              labelText={"Active: " + rule.active}
+                              name="active"
+                              id={index + "_active"}
+                              checked={rule.active}
+                              disabled={rule.active}
+                              onChange={(e) => {
+                                const list = [...ruleList];
+                                list[index]["active"] = e.target.checked;
+                                setRuleList(list);
+                              }}
+                            />
+                          </div>
+                        </Column>
+                      </Grid>
+                    </Column>
+                  </Grid>
                   {rule.toggled && (
                     <>
                       <div className="section">
