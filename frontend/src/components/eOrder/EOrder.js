4a3a4b2c505afcbdfd215aaedeb3fe16dec518ba
saidmurad <saidmurad50@gmail.com>
2026-04-21 14:10:48 +0300

    feat: update incoming orders headers for health ID view

--- frontend/src/components/eOrder/EOrder.js
+++ frontend/src/components/eOrder/EOrder.js
@@ -244,12 +244,6 @@ const EOrder = ({ eOrders, setEOrders, eOrderRef }) => {
               header: intl.formatMessage({ id: "eorder.name.first" }),
             },
             {
-              key: "patientNationalId",
-              header: intl.formatMessage({
-                id: "eorder.id.national",
-              }),
-            },
-            {
               key: "requestingFacility",
               header: intl.formatMessage({
                 id: "eorder.facility.requesting",
@@ -274,21 +268,10 @@ const EOrder = ({ eOrders, setEOrders, eOrderRef }) => {
               }),
             },
             {
-              key: "referringLabNumber",
-              header: intl.formatMessage({
-                id: "eorder.labnumber.referring",
-              }),
-            },
-            {
-              key: "passportNumber",
-              header: intl.formatMessage({
-                id: "eorder.passport.number",
-              }),
-            },
-            {
               key: "subjectNumber",
               header: intl.formatMessage({
-                id: "eorder.id.subjectNumber",
+                id: "eorder.id.healthId",
+                defaultMessage: "Health ID",
               }),
             },
             {
