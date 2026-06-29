f25b57f1a9ce5c3893d3c2ab16b5e89c4484822f
ahmihassu <ahmihassu@gmail.com>
2026-06-29 18:21:25 +0300

    chore(openmrs-payment): remove unused OpenMrsPaymentOrderScope helper
    - Delete unused hasResolvableOrderUuid() method
    - Remove unused OpenMrsOrderUuidResolver injection from order scope
    - Keep isSubjectToPaymentGate() as the sole scope check

--- src/main/java/org/openelisglobal/dataexchange/openmrs/OpenMrsPaymentOrderScope.java
+++ src/main/java/org/openelisglobal/dataexchange/openmrs/OpenMrsPaymentOrderScope.java
@@ -1,9 +1,7 @@
 package org.openelisglobal.dataexchange.openmrs;
 
-import org.apache.commons.validator.GenericValidator;
 import org.openelisglobal.dataexchange.order.valueholder.ElectronicOrder;
 import org.openelisglobal.dataexchange.order.valueholder.ElectronicOrderType;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,14 +11,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class OpenMrsPaymentOrderScope {
 
-    @Autowired
-    private OpenMrsOrderUuidResolver orderUuidResolver;
-
     public boolean isSubjectToPaymentGate(ElectronicOrder electronicOrder) {
         return electronicOrder != null && ElectronicOrderType.FHIR == electronicOrder.getType();
-    }
-
-    public boolean hasResolvableOrderUuid(ElectronicOrder electronicOrder) {
-        return !GenericValidator.isBlankOrNull(orderUuidResolver.resolve(electronicOrder));
     }
 }
